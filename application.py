3e0b626bbcc6c8ec32794970f98b9f00f2ba7a06
Aihab1 <aihab.umair.che19@itbhu.ac.in>
2021-02-08 15:41:10 +0530

    Restructured files & tweaked designs a bit

--- application.py
+++ application.py
@@ -1,36 +1,8 @@
-import os
-
-from flask import Flask, session, render_template, request, redirect, url_for, jsonify
-#from flask_session import Session
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-import connexion
-#import sqlite3
-
-#Import custom table definitions
-from models import *
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-# Create the Connexion application instance
-connex_app = connexion.App(__name__, specification_dir=basedir)
-
-# Get the underlying Flask app instance
-app = connex_app.app
-
-# Configure the SQLAlchemy part of the app instance
-app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'memes.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+from flask import render_template
+from config import *
 
 # Read the swagger.yml file to configure the endpoints
 connex_app.add_api('swagger.yml')
-
-# Create the SQLAlchemy db instance
-db = SQLAlchemy(app)
-
-# Initialize Marshmallow
-ma = Marshmallow(app)
 
 @app.route("/")
 def index():
